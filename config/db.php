550cdcc7ebe25754180eba8ff9e6b7ae53dece0a
nickonyi <nickoluoch1130@gmail.com>
2022-10-05 11:13:23 +0300

    First commit

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+ini_set('display_errors',1);
+define('SITEURL','http://localhost/food-ordering-system/');
+$dbservername = "localhost";
+$dbusername ="root";
+$dbpassword="";
+$dbname= "Linda-Mama";
+
+$conn = mysqli_connect($dbservername,$dbusername,$dbpassword,$dbname); //database connection
+if(!$conn){
+    die("connection failed  ".mysqli_connect_error());
+}
+?>
---
+++ sign/signup.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Sign up page</title>
+</head>
+<body>
+<form>
+  
+  <span>
+  <h2>Sign Up</h2>
+  <br>
+  <label for="name">Name</label>
+  <br>
+  <input id="name" type="text" name="name">
+  <br>
+  <label for="email">Email</label>
+  <br>
+  <input id="email" type="email" name="email">
+  <br>
+  <label for="password">Password</label>
+  <br>
+  <input id="password" type="password" name="password">
+  <br>
+  <input id="button" type="submit" value="Submit">
+  <br>
+  <a href="#">Nevermind</a>
+</form>
+
+    
+</body>
+</html>
