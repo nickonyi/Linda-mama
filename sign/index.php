531dc63166ed5c67d103931f285e668cc6660c57
nickonyi <nickoluoch1130@gmail.com>
2022-10-06 09:13:13 +0300

    4th commit

--- sign/index.php
+++ sign/index.php
@@ -12,16 +12,30 @@
         <div class="header">
             <div class="header-1"><img src="./assets/home-icon.png" alt=""></div>
             <div class="linda-mama-container-header">
-            <div class="header-2"><img src="./assets/Baby-Care.png" alt=""></div> 
+            <div class="header-2"><img src="./assets/Mama-Care.png" alt=""></div> 
             <div class="header-3"><img src="./assets/slasher.png" alt=""></div> 
-            <div class="header-4"><img src="./assets/Mama-Care.png" alt=""></div>
+            <div class="header-4"><img src="./assets/Baby-Care.png" alt=""></div>
             </div>
             <div class="header-5"><img src="./assets/calendar-icon.png" alt=""></div>
         </div>
 
         <div class="main-content">
             <div class="main-content-img">
-                <img class="main-img" src="./assets/main-img.png" alt="">
+            <img class="main-img" src="./assets/main-img.png" alt="">
+                <div class="small-content-img-1">
+                <img class="side-img-1-a" src="./assets/trim.png" alt="">
+                <img class="side-img-2-a" src="./assets/TRIMESTERS.png" alt="">
+                </div>
+                <div class="small-content-img-2">
+                <img class="side-img-1-b" src="./assets/self-d.png" alt="">
+                <img class="side-img-2-b" src="./assets/self-discovery.png" alt="">
+                </div>
+                <div class="small-content-img-3">
+                <img class="side-img-1-c" src="./assets/mental-aware.png" alt="">
+                <img class="side-img-2-c" src="./assets/mental-awareness.png" alt="">
+                </div>
+                <div class="small-content-img-4"></div>
+                
             </div>
         </div>
         
